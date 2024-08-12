6192ccf582a62d1aa6341beb15f004c6c09aee99
Mridul-2003 <110475111+Mridul-2003@users.noreply.github.com>
2024-08-12 23:03:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -113,5 +113,4 @@ def get_nearby_workers():
 #     return {'distance': distance, 'directions': direction}
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port,debug=True)
+    app.run(debug=True)
